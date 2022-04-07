5d053dce5b5784af287a4fb5e670d7afe33fca15
Virgilia Antonucci <virgilia.antonucci@gmail.com>
2022-04-07 19:51:45 -0400

    commit tree d2e8173e7cb65d39177c765708e61daa184c918f

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name) {
+    const copyOfCats = [
+        ...cats, name
+    ]
+    return copyOfCats
+}
+
+function prependCat(name) {
+    const copyOfCats = [
+        name, ...cats
+    ]
+    return copyOfCats
+}
+
+function removeLastCat() {
+    const copyOfCats = [
+        ...cats.slice(0, 2)
+    ]
+    return copyOfCats
+}
+
+function removeFirstCat() {
+    const copyOfCats = [
+        ...cats.slice(1)
+    ]
+    return copyOfCats
+}
